b9faa2246b1ac2241b58e88dbdb2ce461e2aeb70
Vinicius <viniciusmarcelos@gmail.com>
2019-02-01 19:26:41 -0200

    atualizando Chaos

--- ChaoticSort/Program.cs
+++ ChaoticSort/Program.cs
@@ -7,73 +7,68 @@ namespace ChaoticSort
         static void Main(string[] args)
         {
             string[] testArray = new string[] { "ana", "paula", "luiz", "beatriz", "afonso", "beatriz" };
-            char[] vetorchar = testArray[0].ToCharArray();
-            for (int i = 0; i < vetorchar.Length; i++)
-            {
-                Console.WriteLine(vetorchar[i]);
-            }
-            string voltando = vetorchar.ToString();
-            Console.WriteLine(voltando);
-            Console.WriteLine("A maior string entre {0} e {1} é {2}.", testArray[0], testArray[4], Biggerstring(testArray[0], testArray[4]));
-
+            string a = "Anaa           asdsad       asdasd";
+            string b = "Ana";
+            Console.WriteLine("O vetor está em ortem?"+ Is);
 
             Console.ReadKey();
         }
-        //public static string[] Orderstring(string[] vetor)
+        //public static string[] OrderStringArray(string[] array1)
         //{
-
-        //    for (int i = 0; i < vetor.Length; i++)
+        //    string[] array2 = new string[array1.Length];
+        //    if (ordered)
         //    {
-        //        char[] chars1 = vetor[i].ToCharArray();
-        //        char[] chars2 = vetor[i + 1].ToCharArray();
-        //        string maior = "";
-        //        int a = 0;
-        //        do
-        //        {
-        //            if (chars1[a] > chars2[a])
-        //            {
-        //                maior = chars1.ToString();
-        //            }
-        //            else if (chars1[a] < chars2[a])
-        //            {
-        //                maior = chars2.ToString();
-        //            }
-        //            else
-        //            {
-        //                a++;
-        //            }
-        //        }
-        //        while (maior != "");
+        //        return array1;
         //    }
-        //}
-        public static string Biggerstring(string string1, string string2)
+        //    return array2;
+        }
+        public static bool IsItOrdered(string[] a)
         {
-            char[] chars1 = string1.ToCharArray();
-            char[] chars2 = string2.ToCharArray();
-            string bigger = "";
-            int a = 0;
-            int max = chars1.Length > chars2.Length ? chars2.Length : chars1.Length; //usa o tamanho do menor vetor
+            int i = 0;
             do
             {
-                if (chars1[a] > chars2[a])
+                if (IsTheFirstSmaller(a[i],a[i+1]))
                 {
-                    bigger = chars1.ToString();
+                    i++;
                 }
-                else if (chars1[a] < chars2[a])
+                else
                 {
-                    bigger = chars2.ToString();
+                    return false;
+                }
+            }
+            while (i < a.Length-1);
+            return true;
+        }
+
+        public static bool IsTheFirstSmaller(string string1, string string2) //checks if string1 is smaller than string2, returns true or false
+        {
+            bool smaller = true;
+            bool smallerFound = false;
+            int a = 0;
+            int max = string1.Length > string2.Length ? string2.Length : string1.Length; //uses the smaller length vallue
+            do
+            {
+                if (string1[a] > string2[a])
+                {
+                    smaller = false;
+                    smallerFound = true;
+                }
+                else if (string1[a] < string2[a])
+                {
+                    smaller = true;
+                    smallerFound = true;
                 }
                 else
                 {
                     a++;
                 }
             }
-            while (bigger == "" && a < max);
-            if (bigger=="")
+            while (!smallerFound && a < max);
+            if (!smallerFound)
             {
-                bigger = chars1.Length > chars2.Length ? chars1.ToString() : chars2.ToString(); //se todas as letras comparadas foram iguais, a maior string é a com mais letras
+                smaller = string1.Length > string2.Length ? false : true; //if all the compared characters are equal, then the string with less characters is the smaller
             }
-            return bigger;
+            return smaller;
         }
     }
 }
