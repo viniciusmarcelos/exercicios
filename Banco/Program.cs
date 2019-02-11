ad50e5c3a2b47031a50b85a50d4178eeb1758619
Vinicius <viniciusmarcelos@gmail.com>
2019-02-11 20:39:10 -0200

    First commit

---
+++ Banco/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MyLibrary;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] startMenu = new string[] { "Clientes", "Contas", "Operações", "Sair" };
+            StringLib.PrintAddingSpaces("SISTEMA DO BANCO");
+            Console.WriteLine("\nEntre com o número da opção desejada:\n");
+            ArrayLib.PrintMenuStyle1(startMenu);
+            string startMenuChoice = ArrayLib.GetChoiceMenuStyle1(startMenu, Console.ReadLine());
+            switch (startMenuChoice)
+            {
+                case "Clientes":
+                    string[]clientsMenu = new string[] {"Adicionar novo cliente","Remover um cliente","}
+                    break;
+                case "Contas":
+                    break;
+                case "Operações":
+                    break;
+                case "Sair":
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
